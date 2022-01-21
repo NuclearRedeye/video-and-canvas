18ccf20373c972b7de36485233c3222e17c4b13a
NuclearRedeye <github@mneah.com>
2022-01-21 18:39:45 +0000

    fix: change license to MIT

--- html/js/previewbar.js
+++ html/js/previewbar.js
@@ -1,11 +1,3 @@
-/*******************************************************************************
- * Copyright (C) 2014 Joel Summerfield - All Rights Reserved
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- *
- * Authored by Joel Summerfield <joel@mneah.com>
- ******************************************************************************/
 function PreviewBar(id, class_name)
 {
     this.id = id;

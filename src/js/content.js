d1c6364a8713c91278a41712e101760a5a000f93
NuclearRedeye <github@mneah.com>
2022-02-03 19:12:43 +0000

    fix: asset urls

--- src/js/content.js
+++ src/js/content.js
@@ -1,21 +1,22 @@
 var content = 
 [
     {
-        "video_url"         : "./assets/videos/bigbuckbunny.1080p.mov",
-        "poster_url"        : "./assets/videos/bigbuckbunny.1080p.poster.png",
+        "video_url"         : "./assets/videos/bigbuckbunny.1080p.mp4",
+        "poster_url"        : "./assets/images/bigbuckbunny.1080p.poster.png",
         "mosaic_url"        : "./assets/videos/bigbuckbunny.1080p.mosaic.mp4",
-        "mosaic_poster_url" : "./assets/videos/bigbuckbunny.1080p.mosaic.poster.png"
+        "mosaic_poster_url" : "./assets/images/bigbuckbunny.1080p.mosaic.poster.png"
     },
     {
         "video_url"         : "./assets/videos/elephantsdream.1080p.mp4",
-        "poster_url"        : "./assets/videos/elephantsdream.1080p.poster.png",
+        "poster_url"        : "./assets/images/elephantsdream.1080p.poster.png",
         "mosaic_url"        : "./assets/videos/elephantsdream.1080p.mosaic.mp4",
-        "mosaic_poster_url" : "./assets/videos/elephantsdream.1080p.mosaic.poster.png"
+        "mosaic_poster_url" : "./assets/images/elephantsdream.1080p.mosaic.poster.png"
     },
     {
         "video_url"         : "./assets/videos/sintel.1080p.mp4",
-        "poster_url"        : "./assets/videos/sintel.1080p.poster.png",
+        "poster_url"        : "./assets/images/sintel.1080p.poster.png",
         "mosaic_url"        : "./assets/videos/sintel.1080p.mosaic.mp4",
-        "mosaic_poster_url" : "./assets/videos/sintel.1080p.mosaic.poster.png"
+        "mosaic_poster_url" : "./assets/images/sintel.1080p.mosaic.poster.png"
     }
 ];
+ 

d41cd4a530237385c5bdd4b8411419646da6e0ba
NuclearRedeye <github@mneah.com>
2022-02-03 19:41:14 +0000

    refactor: switch to 720p assets

--- src/js/content.js
+++ src/js/content.js
@@ -1,10 +1,10 @@
 var content = 
 [
     {
-        "video_url"         : "./assets/videos/bigbuckbunny.1080p.mp4",
-        "poster_url"        : "./assets/images/bigbuckbunny.1080p.poster.png",
-        "mosaic_url"        : "./assets/videos/bigbuckbunny.1080p.mosaic.mp4",
-        "mosaic_poster_url" : "./assets/images/bigbuckbunny.1080p.mosaic.poster.png"
+        "video_url"         : "./assets/videos/bigbuckbunny.720p.mp4",
+        "poster_url"        : "./assets/images/bigbuckbunny.720p.poster.png",
+        "mosaic_url"        : "./assets/videos/bigbuckbunny.720p.mosaic.mp4",
+        "mosaic_poster_url" : "./assets/images/bigbuckbunny.720p.mosaic.poster.png"
     },
     {
         "video_url"         : "./assets/videos/elephantsdream.1080p.mp4",

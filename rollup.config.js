8e4d152f2b385e1d919901b715e64b4f61de5670
NuclearRedeye <github@mneah.com>
2022-02-13 16:18:01 +0000

    chore: resort to using iife over esm to workaround import issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 export default {
   input: './out/debug/ts/index.js',
-  output: {
-    file: './out/release/index.js',
-    format: 'es',
-    globals: {
-      'react': 'React',
-      'react-dom': 'ReactDOM'
-    }
-  },
+  output: [
+    {
+      file: './out/release/index.iife.js',
+      format: 'iife',
+      globals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM'
+      }
+    },
+    {
+      file: './out/release/index.esm.js',
+      format: 'es',
+    },
+  ],
   external: [ 'react', 'react-dom' ],
 };
